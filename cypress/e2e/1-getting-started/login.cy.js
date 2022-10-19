93af911b99b81ee434cafa268776ff05fb9e990a
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2022-10-20 04:16:01 +0600

    update Create Register page

--- cypress/e2e/1-getting-started/login.cy.js
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -1,11 +1,22 @@
 
 context('Travel App Login', () =>{
-    it('Login page', () =>{
-        cy.visit('https://travel-guru-71.web.app')
-        cy.get('.login-button').click()
+
+    it('Create Register page', () =>{
+    
+        cy.visit('https://travel-guru-71.web.app/login')
+        cy.get('.btn-logintoggle').click()
+        cy.get("input[name=name]").type('Mamun Or Rashid');
+        cy.get("input[name=email]").type('mr1917387@gmail.com');
+        cy.get("input[name=password]").type('mamun421@');
+        cy.get("input[name=confirmPassword]").type('mamun421@');
+        cy.get('.btn-logintoggle').click()  
+    
     })
 
     it('Type email and password and click login', ()=>{
+        cy.visit('https://travel-guru-71.web.app')
+        cy.get('.login-button').click()
+     
         cy.get("input[name=email]").type('mr1917387@gmail.com');
         cy.get("input[name=password]").type("Mamun421@"); 
      
@@ -13,13 +24,15 @@ context('Travel App Login', () =>{
         
     })
     it('Travel Place', ()=>{
+     
         cy.visit('https://travel-guru-71.web.app')
         cy.contains('Booking').click();
     })
     it('Booking Travel Date', ()=>{
+
     
-   
-        cy.wait(10000);
+        cy.get('.booking-form ').type('27/10/2022') ;
+        cy.get('.booking-form').type('02/11/2022');
         cy.contains('Start Booking').click();
     })
 })
---
+++ cypress/e2e/1-getting-started/register.cy.js
@@ -0,0 +1,7 @@
+
+
+// context('Travel App Register', () =>{
+  
+   
+   
+// })
